76da6e5f55cb8da818454022a6ba90f9f85dee2c
XEROXV23 <carlos.vidal0493@gmail.com>
2023-01-29 10:42:43 -0600

    Asignamos el comprobador de semana para leer la carpeta de archivos

--- acumulador.py
+++ acumulador.py
@@ -2,8 +2,10 @@
 import os
 import pandas as pd
 import time
+import openpyxl
 
-# Variables globales
+# Variables globales ( LAS DIRECCIONES DE LAS CARPETAS SERAN VERIFICADAS POR EL NUMERO DE SEMANA ADMINISTRATIVA, asi solo nos preocupamos por cambiar el valor de "week_number")
+
 folder_path = '/home/xeroxv23/Documents/acumulados_sueldos_semanales/SEMANA_{}'
 week_number = 3
 final_path = folder_path.format(week_number)
@@ -12,9 +14,14 @@ acugen_final = acugen_path.format(week_number)
 acugen_excel = "/home/xeroxv23/Documents/acumulados_sueldos_semanales/ACUGEN_SEM_{}/nuevo_acugen.xlsx"
 acugen_excel_final = acugen_excel.format(week_number)
 
-
 # contador de tiempo
 start_time = time.time()
+
+# COMPROBACION SEMANA
+df_cell = pd.read_excel('//home/xeroxv23/Documents/acumulados_sueldos_semanales/SEMANA_3/C-300 LOTE D y E OLMOS.xlsm',sheet_name=0, engine='openpyxl', header=None, skiprows=9, nrows=1, usecols=[2-1])
+df_cell.columns = ['C10']
+value = df_cell.iat[0, 0]
+
 
 # especifica la ruta de la carpeta
 path = final_path
@@ -27,15 +34,21 @@ df_list = []
 
 # itera a través de cada archivo xlsm y lee los datos en un dataframe
 for file in files:
-    df = pd.read_excel(os.path.join(path, file),sheet_name=0, engine='openpyxl', usecols=[0,2,17], header=None)
-    df = df.dropna()
-    df = df.rename(columns={0: 'CODIGO', 2: 'NOMBRE', 17: 'SALARIO'})
+    df_comprobacion = pd.read_excel(os.path.join(path, file),sheet_name=0, engine='openpyxl', header=None, skiprows=9, nrows=1, usecols=[2-1])
+    df_comprobacion.columns = ['C10']
+    value1 = df_comprobacion.iat[0, 0]
+    print(value1)
 
-    df = df[pd.to_numeric(df['CODIGO'], errors='coerce').notnull()]
+    if value1 == week_number:
+        df = pd.read_excel(os.path.join(path, file),sheet_name=0, engine='openpyxl', usecols=[0,2,17], header=None)
+        df = df.dropna()
+        df = df.rename(columns={0: 'CODIGO', 2: 'NOMBRE', 17: 'SALARIO'})
 
-    df['CLAVE_OBRA'] = os.path.basename(file).split(' ')[0]
+        df = df[pd.to_numeric(df['CODIGO'], errors='coerce').notnull()]
 
-    df_list.append(df)
+        df['CLAVE_OBRA'] = os.path.basename(file).split(' ')[0]
+
+        df_list.append(df)
 
 # combina todos los dataframes individuales en un único dataframe
 df_final = pd.concat(df_list)
